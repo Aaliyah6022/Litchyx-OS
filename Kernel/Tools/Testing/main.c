e9b8b6b179e7c6897ce325d70056f6feef5744cd
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-05 13:02:53 +0000

    Update main.c

--- Kernel/Tools/Testing/main.c
+++ Kernel/Tools/Testing/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
 
+#include "Hanyx/mm.h"
+
+#define maximumPages(64)
+
+static void setPages(struct page **pages, const unsigned *array, unsigned num)
+{
+    unsigned int i;
+
+    assert(num < maximumPages);
+
+    for (i = 0; i < num; i++)
+        pages[i] = (struct page *)(unsigned long)
+}
