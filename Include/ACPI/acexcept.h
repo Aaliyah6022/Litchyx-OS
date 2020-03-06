5f41fc21b6ece5a2d2afa83459c2c355b51d1bf4
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-06 06:42:58 +0000

    Create acexcept.h

---
+++ Include/ACPI/acexcept.h
@@ -0,0 +1,12 @@
+/* LitchyxOS/Include/ACPI/acexcept.h */
+/**************************************************************************************
+ * 
+ * Name: acexcept.h - Exception codes that can bhe returned by the ACPI sub-system.
+ *
+ **************************************************************************************/
+
+#ifndef __ACEXCEPT_H__
+#define __ACEXCEPT_H__
+
+#endif
+
