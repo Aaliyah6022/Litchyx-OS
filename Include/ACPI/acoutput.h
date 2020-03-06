2f7e5027513eee27dc72f96c56fb188330b1221a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-06 12:22:56 +0000

    Update acoutput.h

--- Include/ACPI/acoutput.h
+++ Include/ACPI/acoutput.h
@@ -4,3 +4,20 @@
  * Name: acoutput.h - Debug output.
  *
  **************************************************************************************/
+
+#ifndef __ACOUTPUT_H__
+#define __ACOUTPUT_H__
+
+#define ACPI_UTILS              0x00000001
+#define ACPI_HARDWARE           0x00000002
+#define ACPI_EVENTS             0x00000004
+#define ACPI_TABLES             0x00000008
+#define ACPI_NAMESPACES         0x00000010
+#define ACPI_PARSER             0x00000020
+#define ACPI_DISPATCHER         0x00000040
+#define ACPI_EXECUTOR           0x00000080
+#define ACPI_RESOURCES          0x00000100
+#define ACPI_CA_DEBUGGER        0x00000200
+#define ACPI_OS_SERVICE         0x00000400
+#define ACPI_CA_DISASSEMBLER    0x00000800
+
