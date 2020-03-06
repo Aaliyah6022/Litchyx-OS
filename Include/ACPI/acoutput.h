69b5a1d34186e5c816c38735b4ecd84ef674332d
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-06 12:39:01 +0000

    Update acoutput.h

--- Include/ACPI/acoutput.h
+++ Include/ACPI/acoutput.h
@@ -8,6 +8,7 @@
 #ifndef __ACOUTPUT_H__
 #define __ACOUTPUT_H__
 
+// Component IDs are used in the global "DebugLayer".
 #define ACPI_UTILS              0x00000001
 #define ACPI_HARDWARE           0x00000002
 #define ACPI_EVENTS             0x00000004
@@ -20,4 +21,25 @@
 #define ACPI_CA_DEBUGGER        0x00000200
 #define ACPI_OS_SERVICE         0x00000400
 #define ACPI_CA_DISASSEMBLER    0x00000800
+
+// Component IDs for ACPI tools and utilities.
+#define ACPI_COMPILER           0x00001000
+#define ACPI_TOOLS              0x00002000
+#define ACPI_EXAMPLES           0x00004000
+#define ACPI_DRIVER             0x00008000
+#define DT_COMPILER             0x00010000
+#define ASL_PREPROCESSOR        0x00020000
+
+#define ACPI_ALL_COMPONENTS     0x0001FFFF
+#define ACPI_COMPONENT_DEFAULT  (ACPI_ALL_COMPONENTS)
+
+// Component IDs reserved for ACPI drivers.
+#define ACPI_LV_INIT            0x00000001
+#define ACPI_LV_DEBUG_OBJ       0x00000002
+#define ACPI_LV_INFO            0x00000004
+#define ACPI_LV_REPAIR          0x00000008
+#define ACPI_LV_TRACE_POINT     0x00000010
+#define ACPI_LV_ALL_EXCEPTS     0x0000001F
+
+#endif
 
