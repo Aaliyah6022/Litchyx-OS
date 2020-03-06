edb7da9c8ae53588a1214e72f3e5f616306e2930
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-06 12:55:35 +0000

    Update acoutput.h

--- Include/ACPI/acoutput.h
+++ Include/ACPI/acoutput.h
@@ -62,5 +62,81 @@
 #define ACPI_LV_EVALUATE        0x00080000
 #define ACPI_LV_VERBOSITY1      0x000FFF40 | ACPI_LV_ALL_EXCEPTS
 
+/* LitchyxOS/Include/ACPI/acexcept.h */
+/**************************************************************************************
+ *
+ * Name: acoutput.h - Debug output.
+ *
+ **************************************************************************************/
+
+#ifndef __ACOUTPUT_H__
+#define __ACOUTPUT_H__
+
+// Component IDs are used in the global "DebugLayer".
+#define ACPI_UTILS              0x00000001
+#define ACPI_HARDWARE           0x00000002
+#define ACPI_EVENTS             0x00000004
+#define ACPI_TABLES             0x00000008
+#define ACPI_NAMESPACES         0x00000010
+#define ACPI_PARSER             0x00000020
+#define ACPI_DISPATCHER         0x00000040
+#define ACPI_EXECUTOR           0x00000080
+#define ACPI_RESOURCES          0x00000100
+#define ACPI_CA_DEBUGGER        0x00000200
+#define ACPI_OS_SERVICE         0x00000400
+#define ACPI_CA_DISASSEMBLER    0x00000800
+
+// Component IDs for ACPI tools and utilities.
+#define ACPI_COMPILER           0x00001000
+#define ACPI_TOOLS              0x00002000
+#define ACPI_EXAMPLES           0x00004000
+#define ACPI_DRIVER             0x00008000
+#define DT_COMPILER             0x00010000
+#define ASL_PREPROCESSOR        0x00020000
+
+#define ACPI_ALL_COMPONENTS     0x0001FFFF
+#define ACPI_COMPONENT_DEFAULT  (ACPI_ALL_COMPONENTS)
+
+// Component IDs reserved for ACPI drivers.
+#define ACPI_ALL_DRIVERS        0xFFFF0000
+
+// Raw debug output levels, do not use these in the ACPI_DEBUG_PRINT marcos.
+#define ACPI_LV_INIT            0x00000001
+#define ACPI_LV_DEBUG_OBJ       0x00000002
+#define ACPI_LV_INFO            0x00000004
+#define ACPI_LV_REPAIR          0x00000008
+#define ACPI_LV_TRACE_POINT     0x00000010
+#define ACPI_LV_ALL_EXCEPTS     0x0000001F
+
+// Trace verbosity Level 1 [Standard Trace Level]
+#define ACPI_LV_INIT_NAMES      0x00000020
+#define ACPI_LV_PARSE           0x00000040
+#define ACPI_LV_LOAD            0x00000080
+#define ACPI_LV_DISPATCH        0x00000100
+#define ACPI_LV_EXEC            0x00000200
+#define ACPI_LV_NAMES           0x00000400
+#define ACPI_LV_OP_REGION       0x00000800
+#define ACPI_LV_BFIELD          0x00001000
+#define ACPI_LV_TABLES          0x00002000
+#define ACPI_LV_VALUES          0x00004000
+#define ACPI_LV_OBJ             0x00008000
+#define ACPI_LV_RES             0x00010000
+#define ACPI_LV_USER_REQ        0x00020000
+#define ACPI_LV_PACKAGE         0x00040000
+#define ACPI_LV_EVALUATE        0x00080000
+#define ACPI_LV_VERBOSITY1      0x000FFF40 | ACPI_LV_ALL_EXCEPTS
+
+// Trace verbosity level 2 [Function tracing and memory allocation]
+#define ACPI_LV_ALLOCATION      0x00100000
+#define ACPI_LV_FUNC            0x00200000
+#define ACPI_LV_OPTIMIZE        0x00400000
+#define ACPI_LV_PARSETREE       0x00800000
+#define ACPI_LV_VERBOSITY2      0x00F00000 | ACPI_LV_VERBOSITY1
+#define ACPI_LV_ALL             ACPI_LV_VERBOSITY2
+
+#endif
+
+
+
 #endif
 
