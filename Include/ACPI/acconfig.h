d8f9a8e2a9d03799856742b891a352c13ce63ef2
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-06 05:45:50 +0000

    Update acconfig.h

--- Include/ACPI/acconfig.h
+++ Include/ACPI/acconfig.h
@@ -1,12 +1,16 @@
 /* LitchyxOS/Include/ACPI/acconfig.h */
 /*******************************************************
- * 
+ *
+ * Made by: Litchyx Team
+ *
  * Name: acconfig.h - Global configuration constants.
  *
  *******************************************************/
 
 #ifndef __ACCONFIG_H__
 #define __ACCONFIG_H__
+
+#define ACPI_OS_NAME                    "LitchyxOS"
 
 #endif
 
