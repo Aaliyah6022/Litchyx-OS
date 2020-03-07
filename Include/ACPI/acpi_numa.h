66aa20d6347245765e10cf3cc383e7bdd19f0f0b
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-07 07:46:17 +0000

    Create acpi_numa.h

---
+++ Include/ACPI/acpi_numa.h
@@ -0,0 +1,34 @@
+/* LitchyxOS/Include/ACPI/acpi.h */
+/**************************************************************************
+ *
+ * Name: acpi_numa/h
+ *
+ *************************************************************************/
+
+#ifndef __ACPI_NUMA_H__
+#define __ACPI_NUMA_H__
+
+#ifdef CONFIG_ACPI_NUMA
+#include <Litchyx/kernel.h>
+#include <Litchyx/numa.h>
+
+// Proximity bitmap length
+#if MAX_NUMA_NODES > 256
+#define MAX_PXM_DOMAINS                        MAX_NUMA_NODES
+#else
+#define MAX_PXM_DOMAINS (256)
+#endif
+
+extern int pxmToNode(int);
+extern int nodeToPxm(int);
+extern int acpiMapPxmToNode(int);
+extern unsigned char acpiSratRevision;
+extern int acpiNuma __initData;
+
+extern void badSrat(void);
+extern int sratDisabled(void);
+
+#endif
+
+#endif
+
