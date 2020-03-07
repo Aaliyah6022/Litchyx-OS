0cf89a1ddeb2235a264050e4828ef52f552eb673
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-03-07 04:13:02 +0000

    Create acpi.h

---
+++ Include/ACPI/acpi.h
@@ -0,0 +1,23 @@
+/* LitchyxOS/Include/ACPI/acpi.h */
+/**************************************************************************
+ *
+ * Name: acpi.h - Master public include file used to interface ACPICA
+ *
+ *************************************************************************/
+
+#ifndef __ACPI_H__
+#define __ACPI_H__
+
+#include <>
+#include <acnames.h>
+#include <>
+#include <acexcept.h>
+#include <>
+#include <>
+#include <>
+#include <acoutput.h>
+#include <>
+#include <>
+
+#endif
+
